a1c86e6acf87c809653ed6460396a70ea8e60e65
AhmadAlYaaqba <ahmad.y91@hotmail.com>
2018-04-14 09:18:47 +0300

    completed

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -10,7 +10,12 @@ var User = sequelize.define('User', {
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
 
+app.get('/users', function(req, res) {
+	User.findAll().then(function(users) {
+	  res.send(users)
+	})
 
+});
 
 module.exports = { 
   app: app,
